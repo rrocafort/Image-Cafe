18648c26079ddbec8d7178004f47dffec905ae3d
Rainier Rocafort <rrocafort@msn.com>
2022-10-08 00:09:37 -0400

    index.html
    removed <form> element, not necessary

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,24 @@
 $('#fetch').on('click',function(){
-    alert('You have clicked');
+    var userInput = $('#userInput').val();
+    console.log(userInput);
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?api_key=Lub7MmeHO52T5aVFbQ3OPrjQjjUua3Rl&q=${userInput}&limit=30&offset=0&rating=g&lang=en`,
+        dataType: 'JSON',
+        type: 'GET',
+        data: {userInput:userInput},
+        cache: false,
+        success: function(data){
+           $.each(data.data,function(index,value){
+            //console.log(data);
+            let gifTitle = data.data[index].title
+            console.log(gifTitle)
+          
+
+            //console.log(index,value);
+           })
+
+        }
+    })
+    
+
 })
